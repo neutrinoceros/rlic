9b933b0f8094b22f3165c3853b119b0f4cef339c
Clément Robert <cr52@protonmail.com>
2025-02-21 15:56:25 +0100

    first complete implementation (compiles, but crashes)

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use numpy::borrow::{PyReadonlyArray1, PyReadonlyArray2};
 use numpy::ndarray::{Array2, ArrayView1, ArrayView2};
 use numpy::{PyArray2, ToPyArray};
 use pyo3::{pymodule, types::PyModule, Bound, PyResult, Python};
-
+use std::cmp::{max, min};
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
@@ -10,7 +10,64 @@ use pyo3::{pymodule, types::PyModule, Bound, PyResult, Python};
 fn _core<'py>(_py: Python<'py>, m: &Bound<'py, PyModule>) -> PyResult<()> {
     // TODO: parametrize dtype (support f32 too)
 
-    fn advance(vx: f64, vy: f64, x: usize, y: usize, fx: f64, fy: f64, w: usize, h: usize) {}
+    fn advance(
+        vx: f64,
+        vy: f64,
+        x: &mut usize,
+        y: &mut usize,
+        fx: &mut f64,
+        fy: &mut f64,
+        w: usize,
+        h: usize,
+    ) {
+        if (vx == 0.0) && (vy == 0.0) {
+            return;
+        }
+
+        // Think of tx (ty) as the time it takes to reach the next pixel along x (y).
+        let tx: f64 = {
+            -(*fx + {
+                if vx >= 0.0 {
+                    1.0
+                } else {
+                    0.0
+                }
+            }) / vx
+        };
+        let ty: f64 = {
+            -(*fy + {
+                if vy >= 0.0 {
+                    1.0
+                } else {
+                    0.0
+                }
+            }) / vy
+        };
+
+        if tx < ty {
+            // We reached the next pixel along x first.
+            if vx >= 0.0 {
+                *x += 1;
+                *fx = 0.0;
+            } else {
+                *x -= 1;
+                *fx = 1.0;
+            }
+            *fy += tx * vy;
+        } else {
+            // We reached the next pixel along y first.
+            if vy >= 0.0 {
+                *y += 1;
+                *fy = 0.0;
+            } else {
+                *y -= 1;
+                *fy = 1.0;
+            }
+            *fx += ty * vx;
+        }
+        *x = max(0, min(w - 1, *x));
+        *y = max(0, min(h - 1, *y));
+    }
 
     fn convolve<'py>(
         u: ArrayView2<'py, f64>,
@@ -25,10 +82,10 @@ fn _core<'py>(_py: Python<'py>, m: &Bound<'py, PyModule>) -> PyResult<()> {
 
         for i in 0..ny {
             for j in 0..nx {
-                let x = j;
-                let y = i;
-                let fx = 0.5;
-                let fy = 0.5;
+                let mut x = j;
+                let mut y = i;
+                let mut fx = 0.5;
+                let mut fy = 0.5;
                 let mut k = kernellen / 2;
 
                 output[[i, j]] += kernel[[k]] * input[[y, x]];
@@ -36,27 +93,26 @@ fn _core<'py>(_py: Python<'py>, m: &Bound<'py, PyModule>) -> PyResult<()> {
                 while k < kernellen - 1 {
                     let ui = u[[y, x]];
                     let vi = v[[y, x]];
-                    advance(ui, vi, x, y, fx, fy, nx, ny);
+                    advance(ui, vi, &mut x, &mut y, &mut fx, &mut fy, nx, ny);
                     k += 1;
                     output[[i, j]] += kernel[[k]] * input[[y, x]];
                 }
 
-                let x = j;
-                let y = i;
-                let fx = 0.5;
-                let fy = 0.5;
-                k = kernellen / 2;
+                let mut x = j;
+                let mut y = i;
+                let mut fx = 0.5;
+                let mut fy = 0.5;
+                let mut k = kernellen / 2;
 
                 while k > 0 {
                     let ui = u[[y, x]];
                     let vi = v[[y, x]];
-                    advance(-ui, -vi, x, y, fx, fy, nx, ny);
+                    advance(-ui, -vi, &mut x, &mut y, &mut fx, &mut fy, nx, ny);
                     k -= 1;
                     output[[i, j]] += kernel[[k]] * input[[y, x]];
                 }
             }
         }
-        output.fill(1.0);
     }
 
     #[pyfn(m)]
